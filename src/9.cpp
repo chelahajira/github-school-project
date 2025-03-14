614183cfd657ff69992050ff50b599f5ed1b0d36
chelahajira <chelahajira@gmail.com>
2025-03-14 02:43:06 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "You are eligible to vote." << endl;
+    } else {
+        cout << "You are not eligible to vote." << endl;
+    }
+
+    return 0;
+}
