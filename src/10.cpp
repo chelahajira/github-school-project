6b46d6143d6f4043bffccea9a47d5386cdf1331f
chelahajira <chelahajira@gmail.com>
2025-03-15 03:28:48 +0000

    Add 10.cpp

---
+++ src/10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 5, y = 10;
+    if (x > y) {
+        cout << "x is greater than y";
+    } else {
+        cout << "y is greater than x";
+    }
+}
