036b1adad9eb68cd9664fc34639dcf7f08f3c75e
chelahajira <chelahajira@gmail.com>
2025-03-31 08:08:03 +0000

    Add 21.cpp

---
+++ src/21.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+    
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+    
+    if (num1 > num2) {
+        std::cout << num1 << " is greater than " << num2 << "." << std::endl;
+    } else if (num1 < num2) {
+        std::cout << num2 << " is greater than " << num1 << "." << std::endl;
+    } else {
+        std::cout << "Both numbers are equal." << std::endl;
+    }
+    
+    return 0;
+}
