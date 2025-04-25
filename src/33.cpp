6e77d79017d7e4f9feaf446273cf487d2a8bc2b4
chelahajira <chelahajira@gmail.com>
2025-04-25 02:46:33 +0000

    Add 33.cpp

---
+++ src/33.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    if (x > y) {
+        cout << "x is greater than y" << endl;
+    } else {
+        cout << "y is greater than x" << endl;
+    }
+
+    return 0;
+}
